f2b60ade27a8c0d46406cc38f880c6d21950d54d
Freedom <979263784@qq.com>
2023-03-27 18:54:33 +0800

    demo1: init

--- demo2/src/main/kotlin/com/sage/demo2/HelloController.kt
+++ demo2/src/main/kotlin/com/sage/demo2/HelloController.kt
@@ -1,4 +1,30 @@
 package com.sage.demo2
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
 class HelloController {
+
+    @PostMapping("/list")
+    @Operation(summary = "use id find models")
+    fun hello(
+        @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            description = "id list",
+            content = [Content(
+                mediaType = MediaType.APPLICATION_JSON_VALUE,
+                array = ArraySchema(schema = Schema(type = "integer", format = "int64", example = "1"))
+            )]
+        ) @org.springframework.web.bind.annotation.RequestBody ids: List<Long>
+    ): String {
+        return "hello"
+    }
+
 }
