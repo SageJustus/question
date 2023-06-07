1fccd03be5e4e34e277ad1297c41a2efb4f5404b
Freedom <979263784@qq.com>
2023-06-07 10:53:09 +0800

    about next-mdx-remote

---
+++ demo6/src/app/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import {MDXRemote} from 'next-mdx-remote/rsc';
+
+export default async function Home() {
+
+  const resp = await Promise.resolve("Hello World!")
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <MDXRemote source={resp}/>
+    </div>
+  )
+}
