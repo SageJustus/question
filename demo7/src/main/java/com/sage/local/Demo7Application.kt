15d02a725565f733ff525a57adfa878f07b296ec
SageJustus <979263784@qq.com>
2025-04-07 20:01:15 +0800

    demo7

---
+++ demo7/src/main/java/com/sage/local/Demo7Application.kt
@@ -0,0 +1,11 @@
+package com.sage.local
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Demo7Application
+
+fun main(args: Array<String>) {
+    runApplication<Demo7Application>(*args)
+}
