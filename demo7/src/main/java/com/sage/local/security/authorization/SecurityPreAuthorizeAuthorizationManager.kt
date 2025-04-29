0d2e662bd96c2c43c5211931a72e862d77e3c1f2
SageJustus <979263784@qq.com>
2025-04-29 16:20:29 +0800

    demo7 已解决

--- demo7/src/main/java/com/sage/local/security/authorization/SecurityPreAuthorizeAuthorizationManager.kt
+++ demo7/src/main/java/com/sage/local/security/authorization/SecurityPreAuthorizeAuthorizationManager.kt
@@ -5,14 +5,19 @@ import org.springframework.security.authorization.AuthorizationDecision
 import org.springframework.security.authorization.AuthorizationManager
 import org.springframework.security.authorization.method.PreAuthorizeAuthorizationManager
 import org.springframework.security.core.Authentication
+import org.springframework.security.core.annotation.AnnotationTemplateExpressionDefaults
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.stereotype.Component
 import java.util.function.Supplier
 
 @Component
-class SecurityPreAuthorizeAuthorizationManager : AuthorizationManager<MethodInvocation> {
+class SecurityPreAuthorizeAuthorizationManager(
+    val defaults: AnnotationTemplateExpressionDefaults
+): AuthorizationManager<MethodInvocation> {
 
-    private val manager = PreAuthorizeAuthorizationManager()
+    private val manager = PreAuthorizeAuthorizationManager().apply {
+        setTemplateDefaults(defaults)
+    }
 
     override fun check(
         authentication: Supplier<Authentication>,
